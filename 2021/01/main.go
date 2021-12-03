d6c8faf30e8822a8dc754a7231dd24e89f9e490c
James J. Goodhouse <jgoodhouse@newrelic.com>
2021-12-02 18:13:01 -0800

    2021 day 1 consolodate

--- 2021/01/main.go
+++ 2021/01/main.go
@@ -8,52 +8,11 @@ import (
 )
 
 const (
-	inputFile = "input.txt"
+	inputFile  = "input.txt"
+	windowSize = 3
 )
 
 func main() {
-	part1()
-	part2()
-}
-
-func part1() {
-	var increaseCounter, decreaseCounter int
-	var previousValue *int
-
-	file, err := os.Open(inputFile)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	// https://stackoverflow.com/a/16615559/12369692
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		value, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			panic(err)
-		}
-
-		if previousValue != nil {
-			if value > *previousValue {
-				increaseCounter++
-			} else if value < *previousValue {
-				decreaseCounter++
-			}
-		}
-		previousValue = &value
-	}
-
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Part One")
-	fmt.Printf("  Decreased: %d\n", decreaseCounter)
-	fmt.Printf("  Increased: %d\n", increaseCounter)
-}
-
-func part2() {
 	var increaseCounter, decreaseCounter int
 	var previousValue *int
 	var values []int
@@ -79,9 +38,9 @@ func part2() {
 	}
 
 	// https://www.geeksforgeeks.org/window-sliding-technique/
-	for i := 0; i < len(values)-3+1; i++ {
+	for i := 0; i < len(values)-windowSize+1; i++ {
 		value := 0
-		for j := 0; j < 3; j++ {
+		for j := 0; j < windowSize; j++ {
 			value = value + values[i+j]
 		}
 
@@ -95,7 +54,6 @@ func part2() {
 		previousValue = &value
 	}
 
-	fmt.Println("Part Two")
-	fmt.Printf("  Decreased: %d\n", decreaseCounter)
-	fmt.Printf("  Increased: %d\n", increaseCounter)
+	fmt.Printf("Decreased: %d\n", decreaseCounter)
+	fmt.Printf("Increased: %d\n", increaseCounter)
 }
