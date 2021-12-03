613ec01c1c048ccf142ae8b63ff3df4f60d1a444
James J. Goodhouse <jgoodhouse@newrelic.com>
2021-12-03 15:15:54 -0800

    2021 day 3 part 2 WIP

---
+++ 2021/03/go.mod
@@ -0,0 +1,19 @@
+module github.com/jamesgoodhouse/advent-of-code/2021/03
+
+go 1.17
+
+require github.com/ghetzel/go-stockutil v1.10.1
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/structs v1.0.0 // indirect
+	github.com/ghetzel/uuid v0.0.0-20171129191014-dec09d789f3d // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/jbenet/go-base58 v0.0.0-20150317085156-6237cf65f3a6 // indirect
+	github.com/jdkato/prose v1.1.0 // indirect
+	github.com/juliangruber/go-intersect v1.1.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/neurosnap/sentences.v1 v1.0.6 // indirect
+	k8s.io/client-go v11.0.0+incompatible // indirect
+)
--- 2021/03/main.go
+++ 2021/03/main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
+
+	"github.com/ghetzel/go-stockutil/sliceutil"
 )
 
-type ()
-
 const (
-	inputFile = "input.txt"
+	inputFile = "input2.txt"
 )
 
 func main() {
-	bitCount := make([]int, 12)
+	var bitCount []int
+
+	bitStrings := []string{}
 
 	file, err := os.Open(inputFile)
 	if err != nil {
@@ -25,6 +28,13 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		bitString := scanner.Text()
+		bitStrings = append(bitStrings, bitString)
+
+		if bitCount == nil {
+			log.Println("initializing bitCount")
+			bitCount = make([]int, len(bitString))
+		}
+
 		for i := 0; i < len(bitString); i++ {
 			switch bitString[i : i+1] {
 			case "0":
@@ -37,20 +47,70 @@ func main() {
 		}
 	}
 
+	oxStrings := bitStrings
+
 	var gamma string
 	var epsilon string
 
 	for i := range bitCount {
+		log.Println("oxStrings is equal to...")
+		log.Println(oxStrings)
+
+		ox := []string{}
+
 		switch {
 		case bitCount[i] < 0:
+			log.Printf("bit in place '%d' is more commonly 0\n", i)
 			gamma = gamma + "0"
 			epsilon = epsilon + "1"
+
+			for j := range oxStrings {
+				if oxStrings[j][i:i+1] == "0" {
+					if len(oxStrings) > 1 {
+						ox = append(ox, oxStrings[j])
+					}
+				}
+			}
 		case bitCount[i] > 0:
+			log.Printf("bit in place '%d' is more commonly 1\n", i)
 			gamma = gamma + "1"
 			epsilon = epsilon + "0"
+
+			for j := range oxStrings {
+				if oxStrings[j][i:i+1] == "1" {
+					if len(oxStrings) > 1 {
+						ox = append(ox, oxStrings[j])
+					}
+				}
+			}
+		case bitCount[i] == 0:
+			log.Printf("bit in place '%d' is equally 0 and 1", i)
+			for j := range oxStrings {
+				if oxStrings[j][i:i+1] == "1" {
+					if len(oxStrings) > 1 {
+						ox = append(ox, oxStrings[j])
+					}
+				}
+			}
 		default:
 			panic("something's wrong")
 		}
+
+		if i == 4 {
+			log.Println(bitCount)
+		}
+
+		log.Println("ox is equal to...")
+		log.Println(ox)
+
+		if len(ox) > 0 {
+			// oxStrings = Stringify(intersect.Simple(oxStrings, ox))
+			oxStrings = sliceutil.IntersectStrings(oxStrings, ox)
+		}
+
+		log.Println("oxStrings is now equal to...")
+		log.Println(oxStrings)
+		log.Println("")
 	}
 
 	gammaDec, err := strconv.ParseInt(gamma, 2, 64)
@@ -59,4 +119,7 @@ func main() {
 	fmt.Printf("gamma: %s (%d)\n", gamma, gammaDec)
 	fmt.Printf("epsilon: %s (%d)\n", epsilon, epsilonDec)
 	fmt.Printf("multiplied together: %d\n", epsilonDec*gammaDec)
+
+	fmt.Println(oxStrings)
+	// fmt.Printf("oxygen: %s (%d)\n", oxStrings, epsilonDec)
 }
