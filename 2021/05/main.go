7b57b677fe334abf70240f47e5d3cd1e634fd100
James J. Goodhouse <jgoodhouse@newrelic.com>
2021-12-06 15:15:18 -0800

    2021 day 5 part 1 & 2

--- 2021/05/main.go
+++ 2021/05/main.go
@@ -2,23 +2,32 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"regexp"
 	"strconv"
 )
 
 const (
-	inputFile = "input2.txt"
-	regString = `(?P<x1>\d+),(?P<y2>\d+) -> (?P<x2>\d+),(?P<y2>\d+)`
+	inputFile = "input.txt"
+	regString = `(?P<x1>\d+),(?P<y1>\d+) -> (?P<x2>\d+),(?P<y2>\d+)`
 )
 
 type (
-	point  struct{ x, y int }
-	vector struct{ p1, p2 *point }
+	point       struct{ x, y int }
+	lineSegment struct{ p1, p2 *point }
+	grid        [][]int
 )
 
 var (
-	vectors []*vector
+	lineSegments []*lineSegment
+	gridImpl     grid = func() [][]int {
+		g := make([][]int, 1000)
+		for i := range g {
+			g[i] = make([]int, 1000)
+		}
+		return g
+	}()
 )
 
 func main() {
@@ -37,7 +46,7 @@ func main() {
 		groups := re.SubexpNames()
 		matches := re.FindAllStringSubmatch(text, -1)[0]
 
-		l := &vector{
+		l := &lineSegment{
 			p1: &point{},
 			p2: &point{},
 		}
@@ -60,19 +69,75 @@ func main() {
 			}
 		}
 
-		vectors = append(vectors, l)
+		if l.isHorizontal() {
+			if l.p1.x > l.p2.x {
+				_p := l.p1.x
+				l.p1.x = l.p2.x
+				l.p2.x = _p
+			}
+
+			for x := l.p1.x; x <= l.p2.x; x++ {
+				gridImpl[l.p1.y][x]++
+			}
+		} else if l.isVertical() {
+			if l.p1.y > l.p2.y {
+				_p := l.p1.y
+				l.p1.y = l.p2.y
+				l.p2.y = _p
+			}
+
+			for y := l.p1.y; y <= l.p2.y; y++ {
+				gridImpl[y][l.p1.x]++
+			}
+		} else {
+			x := l.p1.x
+			y := l.p1.y
+
+			for {
+				gridImpl[y][x]++
+
+				if x == l.p2.x || y == l.p2.y {
+					break
+				}
+
+				if x >= l.p2.x {
+					x--
+				} else {
+					x++
+				}
+
+				if y >= l.p2.y {
+					y--
+				} else {
+					y++
+				}
+			}
+		}
+
+		lineSegments = append(lineSegments, l)
 	}
+
+	numOverlaps := 0
+	for i := range gridImpl {
+		for j := range gridImpl[i] {
+			if gridImpl[i][j] >= 2 {
+				numOverlaps++
+			}
+		}
+	}
+
+	fmt.Println(numOverlaps)
 }
 
-func (l vector) isVertical() (b bool) {
-	if l.p1.x == l.p2.x {
+func (l lineSegment) isHorizontal() (b bool) {
+	if l.p1.y == l.p2.y {
 		b = true
 	}
 	return
 }
 
-func (l vector) isHorizontal() (b bool) {
-	if l.p1.y == l.p2.y {
+func (l lineSegment) isVertical() (b bool) {
+	if l.p1.x == l.p2.x {
 		b = true
 	}
 	return
