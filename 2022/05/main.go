0a01e9c21453dc6ca047259039bf95b2a8b72fdb
James Goodhouse <jgoodhouse@newrelic.com>
2022-12-06 11:51:35 -0800

    part 1 of 2022 day 5 complete

--- 2022/05/main.go
+++ 2022/05/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"regexp"
+	"strconv"
 	"strings"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/jamesgoodhouse/advent-of-code/2022/helper"
 )
 
 const (
-	inputPath = "input.txt"
-
-	stackWidth      = 3
-	stackSpaceWidth = 1
+	inputPath    = "input.txt"
+	stackSpacing = 4
 )
 
 var ()
@@ -22,8 +22,48 @@ func main() {
 
 	stacks := loadStack(numStacks, maxStackHeight, inputPath)
 
-	for _, row := range stacks {
-		fmt.Printf("%+v\n", row)
+	iLikeToMoveItMoveIt(stacks)
+
+	spew.Dump(stacks)
+}
+
+func iLikeToMoveItMoveIt(stacks [][]*string) {
+	pattern := regexp.MustCompile(`move\s(?P<move>\d+)\sfrom\s(?P<from>\d+)\sto\s(?P<to>\d+)`)
+
+	scanner, inputFile, err := helper.NewFileScanner(inputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer inputFile.Close()
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if !strings.HasPrefix(line, "move") {
+			continue
+		}
+
+		// https://gist.github.com/eculver/d1338aa87e87890e05d4f61ed0a33d6e
+		matches := pattern.FindStringSubmatch(line)
+		subnames := pattern.SubexpNames()
+		if matches == nil || len(matches) != len(subnames) {
+			panic("huh?")
+		}
+
+		instructionsMap := map[string]int{}
+		for i := 1; i < len(matches); i++ {
+			val, err := strconv.Atoi(matches[i])
+			if err != nil {
+				panic("uhh...")
+			}
+			instructionsMap[subnames[i]] = val
+		}
+
+		for i := 0; i < instructionsMap["move"]; i++ {
+			val, newStack := stacks[instructionsMap["from"]-1][0], stacks[instructionsMap["from"]-1][1:]
+			stacks[instructionsMap["from"]-1] = newStack
+			stacks[instructionsMap["to"]-1] = append([]*string{val}, stacks[instructionsMap["to"]-1]...)
+		}
 	}
 }
 
@@ -50,12 +90,12 @@ func findStackDimensions(inputPath string) (int, int) {
 }
 
 func loadStack(numStacks, maxHeight int, input string) [][]*string {
-	stackSlice := make([][]*string, maxHeight)
+	stackSlice := make([][]*string, numStacks)
 	for i := range stackSlice {
-		stackSlice[i] = make([]*string, numStacks)
+		stackSlice[i] = make([]*string, maxHeight)
 	}
 
-	scanner, inputFile, err := helper.NewFileScanner(inputPath)
+	scanner, inputFile, err := helper.NewFileScanner(input)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -69,7 +109,6 @@ func loadStack(numStacks, maxHeight int, input string) [][]*string {
 
 		currentStack := 1
 		currentStackStart := 1
-		stackSpacing := 4
 
 		stackRow := scanner.Text()
 
@@ -78,13 +117,23 @@ func loadStack(numStacks, maxHeight int, input string) [][]*string {
 		for currentStackStart < len(stackRowSlice) {
 			stackValue := strings.Join(stackRowSlice[currentStackStart:currentStackStart+1], "")
 			if strings.TrimSpace(stackValue) != "" {
-				stackSlice[lineNo][currentStack-1] = &stackValue
+				stackSlice[currentStack-1][lineNo] = &stackValue
 			}
 			currentStackStart += stackSpacing
 			currentStack += 1
 		}
 
 		lineNo += 1
+	}
+
+	for stackNum, stack := range stackSlice {
+		newStack := []*string{}
+		for _, item := range stack {
+			if item != nil {
+				newStack = append(newStack, item)
+			}
+		}
+		stackSlice[stackNum] = newStack
 	}
 
 	return stackSlice
---
+++ 2022/go.mod
@@ -0,0 +1,5 @@
+module github.com/jamesgoodhouse/advent-of-code/2022
+
+go 1.19
+
+require github.com/davecgh/go-spew v1.1.1
